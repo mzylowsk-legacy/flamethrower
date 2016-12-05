724602387e9bd1fc9af3144d6e97e15173752f03
Michal Zylowski <zylowskimichal@gmail.com>
2016-12-05 18:21:09 +0100

    Menu works

---
+++ Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+}
