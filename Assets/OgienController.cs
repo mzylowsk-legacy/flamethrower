61ba265c52d83c7b0b450ff554ee3536890837e4
Michal Zylowski <zylowskimichal@gmail.com>
2016-12-05 16:21:22 +0100

    Flamethrower works

---
+++ Assets/OgienController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OgienController : MonoBehaviour {
+
+    public GameObject FlameThrower;
+    public GameObject PoOgien;
+
+    private GameObject paliOgien;
+    public GameObject shapeObject;
+    public GameObject CelObject;
+
+    private float timeLeft = 0.5f;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            paliOgien = Instantiate(FlameThrower);
+            paliOgien.transform.parent = shapeObject.gameObject.transform;
+            paliOgien.transform.position = shapeObject.gameObject.transform.position;
+            paliOgien.transform.rotation = shapeObject.gameObject.transform.rotation;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                kindleFire();
+                timeLeft = 0.5f;
+            }
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var script = paliOgien.GetComponent<DigitalRuby.PyroParticles.FireBaseScript>();
+            script.Stop();
+        }
+    }
+
+    void kindleFire()
+    {
+        var pos = CelObject.transform.position;
+        pos.y = pos.y - 1.0f;
+        Instantiate(PoOgien, pos, new Quaternion());
+    }
+}
---
+++ Assets/PoOgienController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoOgienController : MonoBehaviour {
+
+    public float Duration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Duration <= 0)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+
+        Duration -= Time.deltaTime * 2;
+	}
+}
